c377ddd4ed0078f6c53410efa5c4c109bfc18f46
MarcSky <svarog995@gmail.com>
2022-01-09 09:37:49 +0300

    check folder

--- internal/utils/folder.go
+++ internal/utils/folder.go
@@ -10,24 +10,29 @@ import (
 func CreateOrCheckTranslationFilesFolder(currentPath string) error {
 	var err error
 
-	_, err = os.Stat(currentPath)
-	if os.IsNotExist(err) {
-		err = os.Mkdir(currentPath, os.ModePerm)
-		if err != nil {
-			return errors.Wrap(err, "internal.utils.GenerateDocFolders error with creating folder /docs")
-		}
+	err = CheckFolderExist(currentPath)
+	if err != nil {
+		return errors.Wrap(err, "internal.utils.GenerateDocFolders error with creating folder /docs")
 	}
 
 	var platforms = []string{currentPath + "/ios", currentPath + "/android", currentPath + "/web"}
-	for _, platform := range platforms {
-		_, err = os.Stat(platform)
-		if os.IsNotExist(err) {
-			err = os.Mkdir(platform, os.ModePerm)
-			if err != nil {
-				return errors.Wrapf(err, "internal.utils.GenerateDocFolders error with creating folder %s", platform)
-			}
+	for i := range platforms {
+		err = CheckFolderExist(platforms[i])
+		if err != nil {
+			return errors.Wrapf(err, "internal.utils.GenerateDocFolders error with creating folder %s", platforms[i])
 		}
 	}
 
+	return nil
+}
+
+func CheckFolderExist(path string) error {
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		err = os.Mkdir(path, os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
